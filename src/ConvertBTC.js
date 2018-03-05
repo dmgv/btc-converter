48281f1bfa2430d7d712c6784314fdfe371c3d11
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-03-05 05:04:29 -0300

    Add loader

--- src/ConvertBTC.js
+++ src/ConvertBTC.js
@@ -1,26 +1,29 @@
-const chalk = require("chalk");
-const request = require("request");
+const chalk = require('chalk');
+const request = require('request');
+const ora = require('ora');
 
-function convertBTC(currency = "USD", amount = 1) {
+const spinner = ora({
+  text: 'Retrieving Bitcoin data...',
+  color: 'yellow',
+})
+
+function convertBTC(currency = 'USD', amount = 1) {
   const url = `https://apiv2.bitcoinaverage.com/convert/global?from=BTC&to=${currency}&amount=${amount}`;
+
+  spinner.start();
 
   request(url, (error, response, body) => {
     let apiResponse;
+    spinner.stop();
 
     try {
       apiResponse = JSON.parse(body);
-    } catch (parseError) {
-      console.log(
-        chalk.red("Something went wrong in the API. Try in a few minutes.")
-      );
+    } catch(parseError) {
+      console.log(chalk.red('Something went wrong in the API. Try in a few minutes.'));
       return parseError;
     }
 
-    console.log(
-      `${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(
-        apiResponse.price
-      )}`
-    );
+    console.log(`${chalk.red(amount)} BTC to ${chalk.cyan(currency)} = ${chalk.yellow(apiResponse.price)}`);
   });
 }
 
